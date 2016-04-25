8f21b58526ba629b0d4eeb905e653d5caf036c98
scott meyers <scottmey@gmail.com>
2016-04-25 12:12:29 -0400

    shifts to beta

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 setup(
     name="Layback",
-    version="0.0.2",
+    version="0.0.3",
     description="Command-line tool which creates an animated GIF of a given URLs history.",
     long_description=open('README.md').read(),
     url="http://github.com/scottmey/layback",
@@ -12,18 +12,16 @@ setup(
     author_email="scottmey@gmail.com",
     license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5.1'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
     ],
     keywords="wayback machine archive mementos",
     packages=find_packages(),
     install_requires=['imageio', 'numpy', 'selenium'],
     entry_points={
-        "console_scripts": [ "layback = layback_machine.cli:main" ]
+        "console_scripts": [ "layback = layback.cli:main" ]
     }
 )
